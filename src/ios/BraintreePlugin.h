67fb3bdeb1a7d5310bdaa2e2aee0a3c54e8d1d2a
Adam Lay <adam93.lay@gmail.com>
2016-12-15 14:11:27 +0000

    Fixing imports

--- src/ios/BraintreePlugin.h
+++ src/ios/BraintreePlugin.h
@@ -1,7 +1,7 @@
 #import <Cordova/CDV.h>
-#import <BraintreeCore/BraintreeCore.h>
-#import <BraintreePayPal/BraintreePayPal.h>
-#import <BraintreeDataCollector/BraintreeDataCollector.h>
+#import <Braintree/BraintreeCore.h>
+#import <Braintree/BraintreePayPal.h>
+#import <Braintree/BraintreeDataCollector.h>
 // This is js-native wrapper for Braintree SDK for iOS
 
 @interface BraintreePlugin : CDVPlugin<BTAppSwitchDelegate, BTViewControllerPresentingDelegate>
