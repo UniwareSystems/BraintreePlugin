ee6f0f173f579b4c178a2f25d5b6d55eb12d1b1d
Adam Lay <adam93.lay@gmail.com>
2017-01-06 14:06:27 +0000

    Amending framework paths

--- src/ios/BraintreePlugin.h
+++ src/ios/BraintreePlugin.h
@@ -1,7 +1,7 @@
 #import <Cordova/CDV.h>
-#import <Braintree/BraintreeCore.h>
-#import <Braintree/BraintreePayPal.h>
-#import <Braintree/BraintreeDataCollector.h>
+#import <BraintreeCore/BraintreeCore.h>
+#import <BraintreePayPal/BraintreePayPal.h>
+#import <BraintreeDataCollector/BraintreeDataCollector.h>
 // This is js-native wrapper for Braintree SDK for iOS
 
 @interface BraintreePlugin : CDVPlugin<BTAppSwitchDelegate, BTViewControllerPresentingDelegate>
