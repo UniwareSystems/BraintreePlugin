a331517732b86e7122b944e1f58350fed6dc497b
Adam Lay <adam93.lay@gmail.com>
2018-02-20 10:48:33 +0000

    Adding paypal processor

--- src/android/BraintreePlugin.java
+++ src/android/BraintreePlugin.java
@@ -9,130 +9,61 @@ import org.json.JSONObject;
 
 import com.braintreepayments.api.BraintreeFragment;
 import com.braintreepayments.api.DataCollector;
-import com.braintreepayments.api.PayPal;
 import com.braintreepayments.api.exceptions.InvalidArgumentException;
-import com.braintreepayments.api.interfaces.BraintreeListener;
 import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
 import com.braintreepayments.api.models.PayPalAccountNonce;
-import com.braintreepayments.api.models.PayPalRequest;
 import com.braintreepayments.api.models.PaymentMethodNonce;
 
 import android.app.Activity;
-import android.content.Intent;
 
-public class BraintreePlugin extends CordovaPlugin implements PaymentMethodNonceCreatedListener
-{
-	private CallbackContext callbackContext;
-	private Activity activity = null;
-	private BraintreeFragment mBraintreeFragment = null;
-	protected static final int REQUEST_CODE = 100;
-
-	// @Override
-	// public void onActivityResult(int requestCode, int resultCode, Intent intent)
-	// {
-	// 	super.onActivityResult(requestCode, resultCode, intent);
-	// }
+public class BraintreePlugin extends CordovaPlugin implements PaymentMethodNonceCreatedListener {
+	private CallbackContext _callbackContext;
+	private Activity _activity = null;
+	private BraintreeFragment _braintreeFragment = null;
 
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException
-	{
-		boolean retValue = true;
-		
-		this.callbackContext = callbackContext;
-		this.activity = this.cordova.getActivity();
-		
-		if (action.equals("initWithSettings"))
-		{
-            this.init(args);
-        }
-		else if (action.equals("processImmediate"))
-		{
-        	this.processImmediate(args);
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		_callbackContext = callbackContext;
+		_activity = this.cordova.getActivity();
+
+		if (action.equals("initWithSettings")) {
+			this.init(args);
+		} else if (action.equals("paypalProcessImmediate")) {
+			new PayPalProcessor(_braintreeFragment).processImmediate(args);
+		} else if (action.equals("paypalProcessVaulted")) {
+			new PayPalProcessor(_braintreeFragment).processVaulted(args);
+		} else {
+			return false;
 		}
-		else if (action.equals("processVaulted"))
-		{
-			this.processVaulted(args);
-		}
-		else
-		{
-			retValue = false;
-        }
-		
-        return retValue;
+
+		return true;
 	}
 
-	private void dismiss(JSONArray args)
-	{
-		
+	private void init(JSONArray args) {
+		try {
+			startService(args.getString(0));
+			_callbackContext.success();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void startService(String clientToken)
-	{
-		try
-		{
-			if (this.mBraintreeFragment != null)
-			{
-				this.mBraintreeFragment.removeListener(this);
+	private void startService(String clientToken) {
+		try {
+			if (_braintreeFragment != null) {
+				_braintreeFragment.removeListener(this);
 			}
 
-			this.mBraintreeFragment = BraintreeFragment.newInstance(this.activity, clientToken);
-			this.mBraintreeFragment.addListener(this);
-		}
-		catch (InvalidArgumentException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void init(JSONArray args)
-	{
-		try
-		{
-			startService(args.getString(0));
-			callbackContext.success();
-		}
-		catch (JSONException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	private void processImmediate(JSONArray args)
-	{
-		try
-		{
-			PayPalRequest ppReq = new PayPalRequest(args.getString(0));
-			ppReq.currencyCode(args.getString(1));
-
-			PayPal.requestOneTimePayment(this.mBraintreeFragment, ppReq);
-		}
-		catch (JSONException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void processVaulted(JSONArray args)
-	{
-		try
-		{
-			PayPal.authorizeAccount(this.mBraintreeFragment);
-		}
-		catch (Exception e)
-		{
-			// TODO Auto-generated catch block
+			_braintreeFragment = BraintreeFragment.newInstance(_activity, clientToken);
+			_braintreeFragment.addListener(this);
+		} catch (InvalidArgumentException e) {
 			e.printStackTrace();
 		}
 	}
 
 	@Override
-	public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce)
-	{
-		try
-		{
+	public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
+		try {
 			PayPalAccountNonce pp = (PayPalAccountNonce) paymentMethodNonce;
 
 			JSONObject json = new JSONObject();
@@ -141,25 +72,13 @@ public class BraintreePlugin extends CordovaPlugin implements PaymentMethodNonce
 			json.put("payerId", pp.getPayerId().toString());
 			json.put("payerId", pp.getFirstName().toString());
 			json.put("payerId", pp.getLastName().toString());
-			json.put("deviceData", DataCollector.collectDeviceData(this.mBraintreeFragment));
+			json.put("deviceData", DataCollector.collectDeviceData(_braintreeFragment));
 
 			PluginResult result = new PluginResult(PluginResult.Status.OK, json);
 
-			this.callbackContext.sendPluginResult(result);
-		}
-		catch (Exception e)
-		{
+			_callbackContext.sendPluginResult(result);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public void onCancel(int requestCode)
-	{
-
-	}
-
-	public void onError(Exception error)
-	{
-
 	}
 }
---
+++ src/android/PayPalProcessor.java
@@ -0,0 +1,36 @@
+package com.uniware;
+
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.PayPal;
+import com.braintreepayments.api.models.PayPalRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class PayPalProcessor {
+
+    private BraintreeFragment _braintreeFragment;
+
+    public PayPalProcessor(BraintreeFragment bf) {
+        _braintreeFragment = bf;
+    }
+
+    public void processImmediate(JSONArray args) {
+        try {
+            PayPalRequest ppReq = new PayPalRequest(args.getString(0));
+            ppReq.currencyCode(args.getString(1));
+
+            PayPal.requestOneTimePayment(_braintreeFragment, ppReq);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void processVaulted(JSONArray args) {
+        try {
+            PayPal.authorizeAccount(_braintreeFragment);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
