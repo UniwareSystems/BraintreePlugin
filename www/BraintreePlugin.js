f78f7617c846cbc25fcdbe81f4d891c2428c343e
Adam Lay <adam93.lay@gmail.com>
2018-02-20 11:19:14 +0000

    Plugin js

--- www/BraintreePlugin.js
+++ www/BraintreePlugin.js
@@ -1,59 +1,35 @@
-
-/*
-* PhoneGap plugin for BrainTree payments
-* BraintreePlugin.js
-*
-*
-* by Stephen Hewison
-*/
-
-
-// constructor
-//
-function BraintreePlugin () {
-	console.log('Construct');
-}
-
-// init with environment variables
-//
-BraintreePlugin.prototype.init = function(apiKey, successCallback, failureCallback)
-{
-    console.log('BrainTreePayment.init start');
-    cordova.exec(successCallback, failureCallback, "BraintreePlugin", "initWithSettings", [apiKey])
-    console.log('BrainTreePayment.init done');
-};
-
-BraintreePlugin.prototype.processImmediate = function(amount, currency, env, successCallback, failureCallback)
-{
-  console.log('BrainTreePayment.getCardInfoz start');
-  cordova.exec(successCallback, failureCallback, "BraintreePlugin", "processImmediate", [amount, currency, env])
-  console.log('BrainTreePayment.getCardInfoz done');
-};
-
-BraintreePlugin.prototype.processVaulted = function(env, successCallback, failureCallback)
-{
-  console.log('BrainTreePayment.getCardInfoz start');
-  cordova.exec(successCallback, failureCallback, "BraintreePlugin", "processVaulted", [env])
-  console.log('BrainTreePayment.getCardInfoz done');
-};
-
-// Dismisses Braintree dialog window if developer no more needs it
-//
-BraintreePlugin.prototype.dismiss = function(successCallback, failureCallback)
-{
-	cordova.exec(successCallback, failureCallback, "BraintreePlugin", "dismiss", [])
-  console.log('Dismiss');
-};
-
-// Constructor makes all methods in this file accesible
-// to JavaScript in PhoneGap application
-//
-cordova.addConstructor(function()
-{
+var BraintreePlugin = /** @class */ (function () {
+  function BraintreePlugin() {
+      this.PayPal = new PayPal();
+      this.Cards = new Cards();
+  }
+  BraintreePlugin.prototype.init = function (apiKey, onSuccess, onFailed) {
+      cordova.exec(onSuccess, onFailed, "BraintreePlugin", "initWithSettings", [apiKey]);
+  };
+  BraintreePlugin.prototype.dismiss = function (onSuccess, onFailed) {
+      cordova.exec(onSuccess, onFailed, "BraintreePlugin", "dismiss", []);
+  };
+  return BraintreePlugin;
+}());
+var Cards = /** @class */ (function () {
+  function Cards() {
+  }
+  return Cards;
+}());
+var PayPal = /** @class */ (function () {
+  function PayPal() {
+  }
+  PayPal.prototype.processImmediate = function (amount, currency, env, onSuccess, onFailed) {
+      cordova.exec(onSuccess, onFailed, "BraintreePlugin", "paypalProcessImmediate", [amount, currency, env]);
+  };
+  PayPal.prototype.processVaulted = function (env, onSuccess, onFailed) {
+      cordova.exec(onSuccess, onFailed, "BraintreePlugin", "paypalProcessVaulted", [env]);
+  };
+  return PayPal;
+}());
+cordova.addConstructor(function () {
   if (!window.plugins)
-    window.plugins = {};
-  
+      window.plugins = {};
   if (!window.plugins.braintreeApp)
-    window.plugins.braintreeApp = new BraintreePlugin();
-  
+      window.plugins.braintreeApp = new BraintreePlugin();
 });
---
+++ www/BraintreePlugin.ts
@@ -0,0 +1,35 @@
+declare var cordova: any;
+class BraintreePlugin {
+
+    public PayPal: PayPal = new PayPal();
+    public Cards: Cards = new Cards();
+
+    public init(apiKey: string, onSuccess: Function, onFailed: Function) {
+        cordova.exec(onSuccess, onFailed, "BraintreePlugin", "initWithSettings", [apiKey]);
+    }
+    public dismiss(onSuccess: Function, onFailed: Function) {
+        cordova.exec(onSuccess, onFailed, "BraintreePlugin", "dismiss", []);
+    }
+}
+
+class Cards {
+
+}
+
+class PayPal {
+    public processImmediate(amount, currency, env: string, onSuccess: Function, onFailed: Function) {
+        cordova.exec(onSuccess, onFailed, "BraintreePlugin", "paypalProcessImmediate", [amount, currency, env]);
+    }
+    public processVaulted(env: string, onSuccess: Function, onFailed: Function) {
+        cordova.exec(onSuccess, onFailed, "BraintreePlugin", "paypalProcessVaulted", [env]);
+    }
+}
+
+cordova.addConstructor(function()
+{
+  if (!window.plugins)
+    window.plugins = {};
+  
+  if (!window.plugins.braintreeApp)
+    window.plugins.braintreeApp = new BraintreePlugin();
+});
